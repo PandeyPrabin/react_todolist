73938ae722dc98f25a1cc84c456c8bb1854b8b73
Prabin Pandey <prabinpandey95@gmail.com>
2022-07-20 19:07:47 +0300

    todolist and todoitem created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Addtodo_button from './components/addtodo_button';
+import TodoList from './components/todoList';
 
 function App() {
   return (
     <div className="App">
       <Addtodo_button />
+      <TodoList />
     </div>
   );
 }
---
+++ src/components/todoList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import TodoItem from './todoItem'
+
+function TodoList() {
+  return (
+    <div>
+        <TodoItem />
+    </div>
+  )
+}
+
+export default TodoList
